bf0c8b270c488faa1dd04e2810ca6f66b2aad91b
Lolly <cclolly@gmail.com>
2016-09-19 01:24:10 +0800

    update

--- file.php
+++ file.php
@@ -7,12 +7,12 @@
 #        Email: cclolly@gmail.com
 #     HomePage:
 #      Version: 0.0.1
-#   LastChange: 2016-09-19 01:14:33
+#   LastChange: 2016-09-19 01:23:45
 #      History:
 =============================================================================*/
 
 /**
- * @time 09/19/2016
+ * @time 09/19/2016T01:21
  * @demo rename_file('./a', './b');
  */
 function rename_file($source, $target)

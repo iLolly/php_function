6891d5849df037cef7ce092701f95164ca4bae63
Lolly <cclolly@gmail.com>
2016-09-19 02:03:36 +0800

    add multiple function

--- file.php
+++ file.php
@@ -7,13 +7,14 @@
 #        Email: cclolly@gmail.com
 #     HomePage:
 #      Version: 0.0.1
-#   LastChange: 2016-09-19 01:39:44
+#   LastChange: 2016-09-19 02:01:46
 #      History:
 =============================================================================*/
 
 /**
- * @time 09/19/2016T01:21
- * @demo rename_file('./a', './b');
+ * @time   09/19/2016T01:21
+ * @return (boolean)
+ * @demo   rename_file('./a', './b');
  */
 function rename_file($source, $target)
 {
@@ -23,4 +24,94 @@ function rename_file($source, $target)
     return false;
   }
 }
+
+/**
+ * @param  (string) $path
+ * @return (mixed)
+ */
+function getRequire($path)
+{
+  if (is_file($path)) return require $path;
+}
+
+function put($path, $contents)
+{
+  return file_put_contents($path, $contents);
+}
+
+/**
+ * @time   09/19/2016T01:50
+ * @return (int)
+ */
+function prepend($path, $data)
+{
+  if (file_exists($path)) {
+    return file_put_contents($path, $data . file_get_contents($path));
+  } else {
+    return file_put_contents($path, $data);
+  }
+}
+
+/**
+ * @time   09/19/2016T01:56
+ * @return (int)
+ */
+function append($path, $data)
+{
+  return file_put_contents($path, $data, FILE_APPEND);
+}
+
+/**
+ * @time   09/19/2016T01:58
+ * @return (int)
+ */
+function last_modified($path)
+{
+  return filemtime($path);
+}
+
+/**
+ * @param  (string|array) $paths
+ * @return (int)
+ */
+function delete($paths)
+{
+  $paths = is_array($paths) ? $paths : func_get_args();
+  $count = 0;
+  foreach ($paths as $path) {
+    if (@unlink($path)) {
+      $count += 1;
+    }
+  }
+  return $count;
+}
+
+/**
+ * @time   09/19/2016T01:50
+ * @return (boolean)
+ */
+function move($path, $target)
+{
+  return rename($path, $target);
+}
+
+/**
+ * @time   09/19/2016T01:50
+ * @return (string)
+ */
+function type($path)
+{
+  return filetype($path);
+}
+
+/**
+ * @time   09/19/2016T02:00
+ * @return (int)
+ */
+function size($path)
+{
+  return filesize($path);
+}
+
+/* EOF file.php */
 
